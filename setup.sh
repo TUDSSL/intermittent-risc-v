6684197db900ea09501b093479feea156f64d5f7
Vito Kortbeek <vito.kortbeek@gmail.com>
2022-04-12 17:15:34 +0200

    [noelle] add build

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export PATH="$ROOT_DIR/scripts/:$PATH"
 export PATH="$ROOT_DIR/llvm/llvm-9.0.1/install/bin:$PATH"
+export PATH="$ROOT_DIR/noelle/noelle/install/bin:$PATH"
 export PATH="$ROOT_DIR/icemu/icemu/bin:$PATH"
 
 export LLVM_ROOT="$ROOT_DIR/llvm/llvm-9.0.1/install/"
@@ -13,3 +14,4 @@ export NOELLE="$ROOT_DIR/noelle/noelle/install"
 libgcc_loc=$(arm-none-eabi-gcc -print-libgcc-file-name)
 libgcc_loc=$(dirname "$libgcc_loc")
 export CMAKE_LIBGCC_ARM_BASE_DIR="$libgcc_loc"
+
