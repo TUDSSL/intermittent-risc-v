bbce0779242a34a2ca8de42bc158cd7d13c088f8
Vito Kortbeek <vito.kortbeek@gmail.com>
2022-04-12 16:48:44 +0200

    [scripts] add sourceme

--- setup.sh
+++ setup.sh
@@ -3,3 +3,12 @@
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export PATH="$ROOT_DIR/scripts/:$PATH"
+export PATH="$ROOT_DIR/llvm/llvm-9.0.1/install/bin:$PATH"
+
+export LLVM_ROOT="$ROOT_DIR/llvm/llvm-9.0.1/install/"
+export NOELLE="$ROOT_DIR/noelle/noelle/install"
+
+# For now libgcc is used when compiling the ARM elf files
+libgcc_loc=$(arm-none-eabi-gcc -print-libgcc-file-name)
+libgcc_loc=$(dirname "$libgcc_loc")
+export CMAKE_LIBGCC_ARM_BASE_DIR="$libgcc_loc"
