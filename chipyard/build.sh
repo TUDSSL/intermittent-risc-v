da904f05314f0bfbefb10b2c9166b1f63310e944
Vito Kortbeek <vito.kortbeek@gmail.com>
2022-06-29 13:47:44 +0200

    Add fixes to chipyard build

--- chipyard/build.sh
+++ chipyard/build.sh
@@ -18,5 +18,18 @@ pushd chipyard/toolchains/qemu/roms/edk2
 git branch -m HEAD renamed-HEAD 2> /dev/null
 popd
 
+# Two chipyard generators use relative path symbolic links for the .gitignore
+# This creates warnings during 'git status'
+#   warning: unable to access 'behav/BramDwc/.gitignore': Too many levels of symbolic links
+#   warning: unable to access 'synth/BramDwc/.gitignore': Too many levels of symbolic links
+# Resolve it by copying the .gitignore files
+pushd generators/cva6/src/main/resources/vsrc/cva6/src/fpga-support/behav/BramDwc
+mv ../common/gitignore .gitignore
+popd
+pushd generators/cva6/src/main/resources/vsrc/cva6/src/fpga-support/synth/BramDwc
+mv ../common/gitignore .gitignore
+popd
+
+# Done
 popd
 
