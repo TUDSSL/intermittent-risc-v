3733f06caeceedd5943a165fb271203d6fe9fccb
Vito Kortbeek <vito.kortbeek@gmail.com>
2022-07-18 16:41:21 +0200

    [llvm-toolchain] Add GCC support files (not fully correct)

---
+++ toolchains/riscv/lib/gcc/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/user/intermittent-risc-v/chipyard/chipyard/toolchains/riscv-tools/riscv-gnu-toolchain/build/../riscv-gcc/configure --target=riscv64-unknown-elf --prefix=/home/user/intermittent-risc-v/chipyard/chipyard/riscv-tools-install --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/home/user/intermittent-risc-v/chipyard/chipyard/riscv-tools-install/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=../../riscv-gcc --enable-checking=yes --disable-multilib --with-abi=lp64d --with-arch=rv64imafdc --with-tune=rocket 'CFLAGS_FOR_TARGET=-Os  -mcmodel=medany' 'CXXFLAGS_FOR_TARGET=-Os  -mcmodel=medany'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc" }, { "tune", "rocket" } };
