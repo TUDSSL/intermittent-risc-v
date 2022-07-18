0dcf0d927a08fc7201385dd178fa1da05b3fb221
Vito Kortbeek <vito.kortbeek@gmail.com>
2022-07-18 18:28:22 +0200

    [riscv-toolchain] Update library architecture for files to rv64gc

---
+++ toolchains/riscv/testcode/towers-hack/build/src/CMakeFiles/towers.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/user/intermittent-risc-v/toolchains/riscv/crt.S" "/home/user/intermittent-risc-v/toolchains/riscv/testcode/towers/build/src/CMakeFiles/towers.dir/home/user/intermittent-risc-v/toolchains/riscv/crt.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../src/towers"
+  "../src/portme"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/user/intermittent-risc-v/toolchains/riscv/testcode/towers/src/portme/syscalls.c" "/home/user/intermittent-risc-v/toolchains/riscv/testcode/towers/build/src/CMakeFiles/towers.dir/portme/syscalls.c.obj"
+  "/home/user/intermittent-risc-v/toolchains/riscv/testcode/towers/src/towers/towers_main.c" "/home/user/intermittent-risc-v/toolchains/riscv/testcode/towers/build/src/CMakeFiles/towers.dir/towers/towers_main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/towers"
+  "../src/portme"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
