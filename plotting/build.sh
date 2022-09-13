27c3dc0c154164ec632d39f8f560318f9f0dfd04
Vito Kortbeek <vito.kortbeek@gmail.com>
2022-09-13 10:54:48 +0200

    [plotting] Update plotting scripts

---
+++ plotting/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function run_ipynb {
+    notebook="$1"
+    echo "Running notebook: $notebook"
+    jupyter nbconvert --execute --to html "$notebook"
+}
+
+# Run all the notebooks (that generate the plots)
+run_ipynb PrimaryStoryLine.ipynb
+
+# Copy all the notebooks to a 'notebooks' directory
+mkdir -p notebooks
+mv ./*.html notebooks/
+
