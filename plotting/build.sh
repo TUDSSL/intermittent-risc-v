1303ede72a860c4187881516c9eca5699e6494b2
Vito Kortbeek <vito.kortbeek@gmail.com>
2022-09-14 10:49:41 +0200

    [plotting] Add script to run notebooks from cmd

--- plotting/build.sh
+++ plotting/build.sh
@@ -3,13 +3,14 @@
 function run_ipynb {
     notebook="$1"
     echo "Running notebook: $notebook"
-    jupyter nbconvert --execute --to html "$notebook"
+    #jupyter nbconvert --execute --to html "$notebook"
+    jupyter nbconvert --execute --to notebook "$notebook"
 }
 
 # Run all the notebooks (that generate the plots)
 run_ipynb PrimaryStoryLine.ipynb
 
 # Copy all the notebooks to a 'notebooks' directory
-mkdir -p notebooks
-mv ./*.html notebooks/
+#mkdir -p notebooks
+#mv ./*.html notebooks/
 
